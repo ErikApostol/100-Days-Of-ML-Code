28b6cfde1a3d62fb2739cf0f6721935b971c3a41
ErikApostol <36785240+ErikApostol@users.noreply.github.com>
2018-08-16 15:06:08 +0800

    Update Day1Practice.py

--- Code/Day1Practice.py
+++ Code/Day1Practice.py
@@ -6,4 +6,8 @@ from sklearn.preprocessing import Imputer, LabelEncoder, OneHotEncoder, Standard
 from sklearn.crossvalidation import train_test_split
 
 # Step 2: import data
-df = pd.read_csv("")
+df = pd.read_csv("pima-indians-diabetes.data.csv", header=None) # Do not take any line as header
+X_arr = df.iloc[:, :-1].values # .iloc returns DataFrame, .values returns ndarray
+Y_arr = df.iloc[:, -1].values
+
+# Step 3: impute some values to cells with missing data
